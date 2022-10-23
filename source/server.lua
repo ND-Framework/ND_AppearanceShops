e80085da247cf6ef57f162d30a69544086086fd9
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-10-23 22:26:12 +0200

    fix: version checker.

--- source/server.lua
+++ source/server.lua
@@ -1,6 +1,6 @@
 NDCore = exports["ND_Core"]:GetCoreObject()
 
-NDCore.Functions.VersionChecker("ND_AppearanceShops", GetCurrentResourceName(), "https://github.com/ND-Framework/ND_AppearanceShops", "https://raw.githubusercontent.com/ND-Framework/ND_AppearanceShops/main/ND_Core/fxmanifest.lua")
+NDCore.Functions.VersionChecker("ND_AppearanceShops", GetCurrentResourceName(), "https://github.com/ND-Framework/ND_AppearanceShops", "https://raw.githubusercontent.com/ND-Framework/ND_AppearanceShops/main/fxmanifest.lua")
 
 RegisterNetEvent("ND_ClothingStore:clothingPurchase", function(id)
     local player = source
